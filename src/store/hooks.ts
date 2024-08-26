03dcdddd63c629229b84e67964971d970fdd7e79
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-08-26 08:08:44 -0400

    Added store/hooks

---
+++ src/store/hooks.ts
@@ -0,0 +1,10 @@
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootState} from "./index";
+
+// Use throughout your app instead of plan `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+
+
+
